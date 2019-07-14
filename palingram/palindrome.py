a833d288add4c654f117b4291c020b9a21056aa4
Logan Murphy <nagolyhprum@hotmail.com>
2019-07-13 23:30:52 -0400

    read from file, add main, import, strip

---
+++ palingram/palindrome.py
@@ -0,0 +1,44 @@
+from math import ceil
+#strings are a bunch of characters
+#array is a bunch of things (string is an array too!)
+dictionary = [
+    "malayalam",
+    "tacocat",
+    "bob",
+    "apple",
+    "banana",
+    "racecar",
+    "cherry"
+]
+
+def palindrome(wrd):
+    for i in range(int(ceil(len(wrd)/2))):
+        left = wrd[i]
+        right = wrd[len(wrd)-i-1]
+        if (left != right):
+            return False
+        #print(("\t") + str(i) + " : " + left + ", " + right)
+    return True
+
+
+def main():
+    for wrd in dictionary:
+        if palindrome(wrd):
+            print(wrd)
+
+if __name__ == "__main__":
+    print("from palindrome")
+    main()
+
+# def palindromeWRONG(wrd):
+#     #wrd=apple
+#     for i in range(len(wrd)):
+#         #i=0
+#         left = wrd[i]
+#         #left=a
+#         right = wrd[len(wrd)-i-1]
+#         #right=e
+#         if (left == right):
+#             return True
+#             #print(("\t") + str(i) + " : " + left + ", " + right)
+#     return False
--- palingram/palingram.py
+++ palingram/palingram.py
@@ -1,39 +1,16 @@
-from math import ceil
-#strings are a bunch of characters
-#array is a bunch of things (string is an array too!)
-dictionary = [
-    "malayalam",
-    "tacocat",
-    "bob",
-    "apple",
-    "banana",
-    "racecar",
-    "cherry"
-]
+#from palindrome
+import palindrome
 
-def palindrome(wrd):
-    for i in range(ceil(len(wrd)/2)):
-        left = wrd[i]
-        right = wrd[len(wrd)-i-1]
-        if (left != right):
-            return False
-        #print(("\t") + str(i) + " : " + left + ", " + right)
-    return True
-
-# def palindromeWRONG(wrd):
-#     #wrd=apple
-#     for i in range(len(wrd)):
-#         #i=0
-#         left = wrd[i]
-#         #left=a
-#         right = wrd[len(wrd)-i-1]
-#         #right=e
-#         if (left == right):
-#             return True
-#             #print(("\t") + str(i) + " : " + left + ", " + right)
-#     return False
+def main():
+    d = open("words.txt", "r")
+    for line in d:
+        s = line.strip()
+        if palindrome.palindrome(s):
+            print(s)
 
 
-for wrd in dictionary:
-    if palindrome(wrd):
-        print(wrd)
+
+
+if __name__ == "__main__":
+    print("from palingram")
+    main()
