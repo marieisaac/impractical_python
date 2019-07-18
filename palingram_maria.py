7a4fb27febfbba545a35ec259af302b76c7600e3
marieisaac <marieisaac08@gmail.com>
2019-07-18 10:49:31 -0400

    adding new palingram code

---
+++ palingram_maria.py
@@ -0,0 +1,8 @@
+import palindrome
+
+string= ["nurses run", "stack cats"]
+def palingram():
+  for p in string: # strip spaces between words--> make palindrome-ish
+        l= p.replace(" ", "")
+    palindrom.main():
+return True
